5e21c7bf891631106057d8878abcb5fc318ac39b
willerf <73970997+willerf@users.noreply.github.com>
2021-08-20 21:04:02 -0700

    file upload

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import axios from 'axios';
+
+export default class Home extends Component {
+
+ state = { selectedFile: null }
+ 
+  fileChangedHandler = event => {
+    this.setState({ selectedFile: event.target.files[0] })
+  }
+
+
+
+  uploadHandler = () => {
+    console.log(this.state.selectedFile)
+    const data = new FormData()
+    data.append(
+      'test',
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    )
+
+    axios.post('http://localhost:3001/file-upload', data).then(data => {
+      console.log(data);
+
+    })
+  }
+
+ 
+
+ render(){
+  return(
+    <div>
+      <input type="file" onChange={this.fileChangedHandler}></input>
+      <button onClick={this.uploadHandler}>Upload!</button>
+    </div>
+    )
+  }
+
+}
